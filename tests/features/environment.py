d18b8ba5138a4b064d5441a6bcd1e4a74f823277
Siddharth Thakur <siddharth9126@gmail.com>
2020-08-06 12:44:34 +0530

    environment updated

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -3,23 +3,25 @@ from selenium import webdriver
 import os
 from pages.common import Common
 
+
 # Run Before the Feature testing execution
 def before_all(context):
-    option = webdriver.ChromeOptions()
-    option.add_argument(" — incognito")
-    option.add_argument("--headless")
-    chrome_dir = '' + os.getcwd() + '/pages/chromedriver'
-    context.driver = webdriver.Chrome(executable_path = chrome_dir, options = option)
+    context.option = webdriver.ChromeOptions()
+    context.option.add_argument(" — incognito")
+    context.option.add_argument("--headless")
+    context.chrome_dir = '' + os.getcwd() + '/pages/chromedriver'
 
 
 # Run before every scenario of the feature
 def before_scenario(context, sceanrio):
-    context.common = Common(context.driver)
+    sceanrio.driver = webdriver.Chrome(executable_path = context.chrome_dir, options = context.option)
+    context.common = Common(sceanrio.driver)
 
 
-# Run after all scenarios are tested
-def after_all(context):
+# Run after each scenarios are tested
+def after_scenario(context, scenario):
     # close browsers
     context.common.browser_close()
+
 
 
